9d7bd3a40a7d94e826ae6e8674e9e88d02fc58d6
Meghan Bucher <115611931+megbuch@users.noreply.github.com>
2025-09-27 18:13:27 -0700

    Add path import.

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 require('dotenv').config()
 const express = require('express')
+const path = require('path')
 const app = express()
 const { initializeDatabase } = require('./config/database')
 
@@ -14,16 +15,17 @@ app.use('/api/activities', require('./routes/activities'))
 app.use('/api/bookings', require('./routes/bookings'))
 app.use('/api/unsplash', require('./routes/unsplash'))
 
-const port = process.env.PORT || 3001
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`)
-})
-
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')))
   app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../client/build/index.html'))
   })
 }
+
+const port = process.env.PORT || 3001
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`)
+})
+
 
 initializeDatabase()
